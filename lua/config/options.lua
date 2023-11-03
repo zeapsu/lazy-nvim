5e0fd30c0e85b9e33ab69cb274285f2153167384
zeapsu <andryypaez@gmail.com>
2023-11-02 22:17:58 -0700

    extras

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,5 +2,17 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 local opt = vim.opt
-opt.winbar = "%=%m %f"
+local g = vim.g
+
+-- nvim options
+-- opt.winbar = "%=%m %f"
+opt.swapfile = false
 opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.textwidth = 80
+-- opt.colorcolumn = "+1"
+opt.columns = 80
+
+-- plugin options
+g.vimtex_view_method = "sioyek"
