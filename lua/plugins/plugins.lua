66dc81cc26e4d438d4b32fde81d97c07ad404cde
zeapsu <andryypaez@gmail.com>
2024-02-18 16:07:51 -0800

    remove redundant import

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -5,7 +5,6 @@
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
-  { "github/copilot.vim", lazy = false },
   { "KeitaNakamura/tex-conceal.vim", event = "BufEnter *.tex" },
   { "matze/vim-tex-fold", event = "BufEnter *.tex" },
 }
