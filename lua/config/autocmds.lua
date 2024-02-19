f7fd55804fb82d64222987785f08a4445573719d
zeapsu <andryypaez@gmail.com>
2024-02-18 16:20:22 -0800

    change tabspacing and shift width for web development to 2

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+--
+-- Set tab spacing for specific file types
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "html", "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  callback = function()
+    vim.opt_local.shiftwidth = 2
+    vim.opt_local.tabstop = 2
+    vim.opt_local.expandtab = true
+  end,
+})
