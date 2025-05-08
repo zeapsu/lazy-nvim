9db85331255198db83b8056e1339f00ca9ceff86
zeapsu <andryypaez@gmail.com>
2025-05-08 02:14:35 -0700

    new changes, idk what though it's been too long

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -18,39 +18,39 @@ return {
   },
 
   -- override the configuration of Copilot plugins
-  {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    build = ":Copilot auth",
-    opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-      filetypes = {
-        markdown = true,
-        lua = true,
-        help = true,
-      },
-    },
-  },
-  {
-    "zbirenbaum/copilot-cmp",
-    dependencies = "copilot.lua",
-    opts = {
-      event = { "InsertEnter", "LspAttach" },
-      fix_pairs = false,
-    },
-    config = function(_, opts)
-      local copilot_cmp = require("copilot_cmp")
-      copilot_cmp.setup(opts)
-      -- attach cmp source whenever copilot attaches
-      -- fixes lazy-loading issues with the copilot cmp source
-      require("lazyvim.util").lsp.on_attach(function(client)
-        if client.name == "copilot" then
-          copilot_cmp._on_insert_enter({})
-        end
-      end)
-    end,
-  },
+  -- {
+  --   "zbirenbaum/copilot.lua",
+  --   cmd = "Copilot",
+  --   build = ":Copilot auth",
+  --   opts = {
+  --     suggestion = { enabled = false },
+  --     panel = { enabled = false },
+  --     filetypes = {
+  --       markdown = true,
+  --       lua = true,
+  --       help = true,
+  --     },
+  --   },
+  -- },
+  -- {
+  --   "zbirenbaum/copilot-cmp",
+  --   dependencies = "copilot.lua",
+  --   opts = {
+  --     event = { "InsertEnter", "LspAttach" },
+  --     fix_pairs = false,
+  --   },
+  --   config = function(_, opts)
+  --     local copilot_cmp = require("copilot_cmp")
+  --     copilot_cmp.setup(opts)
+  --     -- attach cmp source whenever copilot attaches
+  --     -- fixes lazy-loading issues with the copilot cmp source
+  --     require("lazyvim.util").lsp.on_attach(function(client)
+  --       if client.name == "copilot" then
+  --         copilot_cmp._on_insert_enter({})
+  --       end
+  --     end)
+  --   end,
+  -- },
   -- setup the supertab for cmp
   {
     "hrsh7th/nvim-cmp",
@@ -95,4 +95,5 @@ return {
     end,
   },
   { "normen/vim-pio" },
+  { "andweeb/presence.nvim" },
 }
