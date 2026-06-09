e63070b38b2e24d8f0c24815c10d8161d6e07d0a
zeapsu <andryypaez@gmail.com>
2026-06-08 17:07:34 -0700

    fix: set ltex JAVA_OPTS to resolve jdk.xml.totalEntitySizeLimit error in java 24

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -4,6 +4,9 @@ return {
     opts = {
       servers = {
         ltex = {
+          cmd_env = {
+            JAVA_OPTS = "-Djdk.xml.totalEntitySizeLimit=50000000",
+          },
           settings = {
             ltex = {
               ["ltex-ls"] = {
